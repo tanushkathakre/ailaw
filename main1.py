25dcb24f5a2be8cb0bdf6f8f6fd898fde9d34ca0
tanushkathakre <104820098+tanushkathakre@users.noreply.github.com>
2024-04-19 19:59:40 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,4 +1,4 @@
-import predict_winning_percentage from bert
+from bert import predict_winning_percentage 
 import streamlit as st
 respondent_percentage, petitioner_percentage = predict_winning_percentage(the_model_1_2, 'case.txt')
 print(f'Respondent winning percentage: {respondent_percentage}%')
